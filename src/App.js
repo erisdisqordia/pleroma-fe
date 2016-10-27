7ba40ff7e21de4622671f7da1e9210f7380b9341
Roger Braun <roger@rogerbraun.net>
2016-10-27 18:01:48 +0200

    Remove hello component from app.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Hello from './components/hello/Hello'
+import UserPanel from './components/user_panel/user_panel.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    UserPanel
   }
 }

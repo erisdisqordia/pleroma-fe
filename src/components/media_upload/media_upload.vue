a73916dd7f07954ee0305df822baa8068ae1df14
Roger Braun <roger@rogerbraun.net>
2016-11-06 19:28:37 +0100

    Add MediaUpload component.

---
+++ src/components/media_upload/media_upload.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="media-upload">
+    <label class="btn btn-default">
+      <i class="fa icon-upload"></i>
+      <input type=file style="position: fixed; top: -100em"></input>
+    </label>
+  </div>
+</template>
+
+<script src="./media_upload.js" ></script>
+
+<style>
+ .media-upload {
+     font-size: 26px;
+     flex: 1;
+ }
+</style>

15c20e60328af11c5e7fd5827887cd362668cc3d
Roger Braun <roger@rogerbraun.net>
2016-11-07 18:03:36 +0100

    Unify App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
-<template src="./App.html"></template>
+<template>
+  <div id="app" v-bind:style="style">
+    <nav class='container'>
+      <div class='item'>
+        <a route-to='friends-timeline' href="#">Pleroma FE</a>
+      </div>
+    </nav>
+    <div class="container" id="content">
+      <sidebar>
+        <user-panel></user-panel>
+        <nav-panel></nav-panel>
+      </sidebar>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
 
 <script src="./App.js"></script>
 <style lang="scss" src="./App.scss"></style>

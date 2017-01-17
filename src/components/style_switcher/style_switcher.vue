3be7a3ea85a5fcc6f3eacf8ddeb1fe007573aee4
Roger Braun <roger@rogerbraun.net>
2017-01-17 14:41:13 +0100

    Align style switcher.

--- src/components/style_switcher/style_switcher.vue
+++ src/components/style_switcher/style_switcher.vue
@@ -1,7 +1,13 @@
 <template>
-  <select v-model="selected">
+  <select v-model="selected" class="style-switcher">
     <option v-for="style in availableStyles" >{{style}}</option>
   </select>
 </template>
 
 <script src="./style_switcher.js"></script>
+
+<style lang="scss">
+ .style-switcher {
+   margin-right: 1em;
+ }
+</style>

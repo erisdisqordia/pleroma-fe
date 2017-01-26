5a518fa817fbd64fcf9b3a18b02acefcff5fc307
Roger Braun <roger@rogerbraun.net>
2017-01-26 12:03:07 +0100

    Break after user name in notifications.

--- src/components/notifications/notifications.vue
+++ src/components/notifications/notifications.vue
@@ -9,15 +9,15 @@
           </a>
           <div class='text'>
             <div v-if="notification.type === 'favorite'">
-              <h1>{{ notification.action.user.name }}<i class="fa icon-star"></i> favorited your <router-link :to="{ name: 'conversation', params: { id: notification.status.id } }">status</h1>
+              <h1>{{ notification.action.user.name }}<br><i class="fa icon-star"></i> favorited your <router-link :to="{ name: 'conversation', params: { id: notification.status.id } }">status</h1>
               <p>{{ notification.status.text }}</p>
             </div>
             <div v-if="notification.type === 'repeat'">
-              <h1>{{ notification.action.user.name }}<i class="fa icon-retweet"></i> repeated your <router-link :to="{ name: 'conversation', params: { id: notification.status.id } }">status</h1>
+              <h1>{{ notification.action.user.name }}<br><i class="fa icon-retweet"></i> repeated your <router-link :to="{ name: 'conversation', params: { id: notification.status.id } }">status</h1>
               <p>{{ notification.status.text }}</p>
             </div>
             <div v-if="notification.type === 'mention'">
-              <h1>{{ notification.action.user.name }}<i class="fa icon-reply"></i> <router-link :to="{ name: 'conversation', params: { id: notification.status.id } }">mentioned</router-link> you</h1>
+              <h1>{{ notification.action.user.name }}<br><i class="fa icon-reply"></i> <router-link :to="{ name: 'conversation', params: { id: notification.status.id } }">mentioned</router-link> you</h1>
               <p>{{ notification.status.text }}</p>
             </div>
           </div>

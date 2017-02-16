52dd7c009f12e1d5522787f4173670c408efa158
Roger Braun <roger@rogerbraun.net>
2017-02-16 22:25:29 +0100

    Add Settings page.

---
+++ src/components/settings/settings.js
@@ -0,0 +1,9 @@
+import StyleSwitcher from '../style_switcher/style_switcher.vue'
+
+const settings = {
+  components: {
+    StyleSwitcher
+  }
+}
+
+export default settings
---
+++ src/components/settings/settings.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="settings panel panel-default base00-background">
+    <div class="panel-heading base01-background base04">
+      Settings
+    </div>
+    <div class="panel-body">
+      <div class="setting-item">
+        <h2>Theme</h2>
+        <style-switcher></style-switcher>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./settings.js">
+</script>
+
+<style>
+ .setting-item {
+   margin: 1em
+ }
+</style>

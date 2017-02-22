6c385ae80aaf763db7f973b5dc1e3bd21367a2a7
shpuld <shpuld@gmail.com>
2017-02-22 14:53:05 +0200

    Make drag&drop work on more browsers/platforms, make the Upload button accept drops as well.

--- src/components/media_upload/media_upload.vue
+++ src/components/media_upload/media_upload.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="media-upload">
+  <div class="media-upload" @drop.prevent @dragover.prevent="fileDrag" @drop="fileDrop">
     <label class="btn btn-default">
       <i class="fa icon-spin4 animate-spin" v-if="uploading"></i>
       <i class="fa icon-upload" v-if="!uploading"></i>

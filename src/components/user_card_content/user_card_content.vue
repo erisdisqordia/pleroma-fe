8c43b3f505c6bee344a85dc33a927f123062a268
shpuld <shpuld@gmail.com>
2017-02-26 22:42:22 +0200

    Remove newlines from template literal.

--- src/components/user_card_content/user_card_content.vue
+++ src/components/user_card_content/user_card_content.vue
@@ -63,9 +63,7 @@
       headingStyle () {
         let rgb = this.$store.state.config.colors['base00'].match(/\d+/g)
         return {
-          backgroundColor: `rgb(${Math.floor(rgb[0] * 0.53)},
-                                ${Math.floor(rgb[1] * 0.56)},
-                                ${Math.floor(rgb[2] * 0.59)})`,
+          backgroundColor: `rgb(${Math.floor(rgb[0] * 0.53)}, ${Math.floor(rgb[1] * 0.56)}, ${Math.floor(rgb[2] * 0.59)})`,
           backgroundImage: `url(${this.user.cover_photo})`
         }
       },

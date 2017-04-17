b3b6d43dcc56ba8828d4bd180676b46141c0f0b1
Shpuld Shpuldson <shpuld@gmail.com>
2017-04-17 13:57:02 +0300

    Add ToS field into registration form (still need to make it fetch it somewhere.) Make the register form look more polished.

--- src/components/registration/registration.vue
+++ src/components/registration/registration.vue
@@ -5,32 +5,40 @@
     </div>
     <div class="panel-body">
       <form v-on:submit.prevent='submit(user)' class='registration-form'>
-        <div class='form-group'>
-          <label for='username'>Username</label>
-          <input :disabled="registering" v-model='user.username' class='form-control' id='username' placeholder='e.g. lain'>
-        </div>
-        <div class='form-group'>
-          <label for='fullname'>Fullname</label>
-          <input :disabled="registering" v-model='user.fullname' class='form-control' id='fullname' placeholder='e.g. Lain Iwakura'>
-        </div>
-        <div class='form-group'>
-          <label for='email'>Email</label>
-          <input :disabled="registering" v-model='user.email' class='form-control' id='email' type="email">
-        </div>
-        <div class='form-group'>
-          <label for='bio'>Bio</label>
-          <input :disabled="registering" v-model='user.bio' class='form-control' id='bio'>
-        </div>
-        <div class='form-group'>
-          <label for='password'>Password</label>
-          <input :disabled="registering" v-model='user.password' class='form-control' id='password' type='password'>
-        </div>
-        <div class='form-group'>
-          <label for='password_confirmation'>Password confirmation</label>
-          <input :disabled="registering" v-model='user.confirm' class='form-control' id='password_confirmation' type='password'>
-        </div>
-        <div class='form-group'>
-          <button :disabled="registering" type='submit' class='btn btn-default base05 base01-background'>Submit</button>
+        <div class='container'>
+          <div class='text-fields'>
+            <div class='form-group'>
+              <label for='username'>Username</label>
+              <input :disabled="registering" v-model='user.username' class='form-control' id='username' placeholder='e.g. lain'>
+            </div>
+            <div class='form-group'>
+              <label for='fullname'>Fullname</label>
+              <input :disabled="registering" v-model='user.fullname' class='form-control' id='fullname' placeholder='e.g. Lain Iwakura'>
+            </div>
+            <div class='form-group'>
+              <label for='email'>Email</label>
+              <input :disabled="registering" v-model='user.email' class='form-control' id='email' type="email">
+            </div>
+            <div class='form-group'>
+              <label for='bio'>Bio</label>
+              <input :disabled="registering" v-model='user.bio' class='form-control' id='bio'>
+            </div>
+            <div class='form-group'>
+              <label for='password'>Password</label>
+              <input :disabled="registering" v-model='user.password' class='form-control' id='password' type='password'>
+            </div>
+            <div class='form-group'>
+              <label for='password_confirmation'>Password confirmation</label>
+              <input :disabled="registering" v-model='user.confirm' class='form-control' id='password_confirmation' type='password'>
+            </div>
+            <div class='form-group'>
+              <button :disabled="registering" type='submit' class='btn btn-default base05 base01-background'>Submit</button>
+            </div>
+          </div>
+          <div class='terms-of-service'>
+            <h4>Terms of Service</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
         </div>
         <div v-if="error" class='form-group'>
           <div class='error base05'>{{error}}</div>
@@ -44,28 +52,45 @@
 <style lang="scss">
 
 .registration-form {
-     form {
-         display: flex;
-         flex-direction: column;
-         padding: 0.6em;
-     }
+  display: flex;
+  flex-direction: column;
+  margin: 0.6em;
 
-     .form-group {
-         display: flex;
-         flex-direction: column;
-         padding: 0.3em 0.5em 0.6em;
-         line-height:24px;
-     }
+  .container {
+    display: flex;
+    flex-direction: row;
+    //margin-bottom: 1em;
+  }
 
-     form textarea {
-         border: solid;
-         border-width: 1px;
-         border-color: silver;
-         border-radius: 5px;
-         line-height:16px;
-         padding: 5px;
-         resize: vertical;
-     }
+  .terms-of-service {
+    flex: 0 1 55%;
+    margin: 0.8em;
+  }
+
+  .text-fields {
+    margin-top: 0.6em;
+    flex: 1 0;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    padding: 0.3em 0.0em 0.3em;
+    line-height:24px;
+  }
+
+  form textarea {
+    border: solid;
+    border-width: 1px;
+    border-color: silver;
+    border-radius: 5px;
+    line-height:16px;
+    padding: 5px;
+    resize: vertical;
+  }
+
   input {
     border-width: 1px;
     border-style: solid;
@@ -75,16 +100,25 @@
   }
 
   .btn {
-    margin-top: 1.0em;
-    min-height: 28px;
+    //align-self: flex-start;
+    //width: 10em;
+    margin-top: 0.6em;
+    height: 28px;
   }
 
   .error {
     border-radius: 5px;
     text-align: center;
+    margin: 0.5em 0.6em 0;
     background-color: rgba(255, 48, 16, 0.65);
     min-height: 28px;
     line-height: 28px;
+  }
+}
+
+@media all and (max-width: 959px) {
+  .registration-form .container {
+    flex-direction: column-reverse;
   }
 }
 </style>

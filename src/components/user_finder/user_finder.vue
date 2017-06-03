bac1d3bd57fb0c04b32c31a048ccf58a3dee6be0
Shpuld Shpuldson <shpuld@gmail.com>
2017-06-03 23:42:54 +0300

    Move user-finder into navbar and hide it behind a toggle button, update fontello icons with user-plus and menu.

--- src/components/user_finder/user_finder.vue
+++ src/components/user_finder/user_finder.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="user-finder-panel panel panel-default base00-background">
-    <input @keyup.enter="findUser(username)" v-model="username" placeholder="Find user" id="user-finder-input" type="text" />
-  </div>
+  <a href="#" v-if="hidden"><i class="icon-user-plus user-finder-icon" @click.prevent="toggleHidden"/></a>
+  <span v-else>
+    <input class="user-finder-input base03-border" @keyup.enter="findUser(username)" v-model="username" placeholder="Find user" id="user-finder-input" type="text"/>
+    <i class="icon-cancel user-finder-icon" @click="toggleHidden"/>
+  </span>
 </template>
 
 <script src="./user_finder.js"></script>
 
 <style lang="scss">
- .user-finder-panel {
-   padding: 10px;
+ .user-finder-icon {
+   margin-right: 0.25em;
+ }
+
+ .user-finder-input {
+   border-width: 1px;
+   border-style: solid;
+   border-color: inherit;
+   border-radius: 5px;
+   padding: 0.1em 0.2em 0.2em 0.2em;
  }
 </style>

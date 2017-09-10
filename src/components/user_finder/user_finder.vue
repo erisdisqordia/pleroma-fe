5e6ffc0e45abba5e7519457b21bd63ae9ba008f8
eal <eal@waifu.club>
2017-09-10 20:46:42 +0300

    Make user finder more pleasant.

--- src/components/user_finder/user_finder.vue
+++ src/components/user_finder/user_finder.vue
@@ -1,8 +1,15 @@
 <template>
-  <a href="#" v-if="hidden"><i class="icon-user-plus user-finder-icon" @click.prevent="toggleHidden"/></a>
-  <span v-else>
-    <input class="user-finder-input base03-border" @keyup.enter="findUser(username)" v-model="username" placeholder="Find user" id="user-finder-input" type="text"/>
+  <span>
+    <span class="finder-error base05" v-if="error">
+      <i class="icon-cancel user-finder-icon" @click="dismissError"/>
+      Error fetching user
+    </span>
+    <i class="icon-spin4 user-finder-icon animate-spin-slow" v-if="loading" />
+    <a href="#" v-if="hidden"><i class="icon-user-plus user-finder-icon" @click.prevent="toggleHidden"/></a>
+    <span v-else>
+      <input class="user-finder-input base03-border" @keyup.enter="findUser(username)" v-model="username" placeholder="Find user" id="user-finder-input" type="text"/>
     <i class="icon-cancel user-finder-icon" @click="toggleHidden"/>
+    </span>
   </span>
 </template>
 
@@ -19,5 +26,12 @@
    border-color: inherit;
    border-radius: 5px;
    padding: 0.1em 0.2em 0.2em 0.2em;
+ }
+
+ .finder-error {
+   background-color: rgba(255, 48, 16, 0.65);
+   margin: 0.35em;
+   border-radius: 5px;
+   padding: 0.25em;
  }
 </style>

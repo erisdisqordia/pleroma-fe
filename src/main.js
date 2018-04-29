b1d1c7cfc4a58fa508d2b2e2f092c040c23d99d0
Hakaba Hitoyo <example@example.com>
2018-04-29 18:07:40 +0900

    lint

--- src/main.js
+++ src/main.js
@@ -98,10 +98,12 @@ window.fetch('/static/config.json')
     }
 
     const routes = [
-      { name: 'root', path: '/', redirect: to => {
-        var redirectRootLogin = data['redirectRootLogin']
-        var redirectRootNoLogin = data['redirectRootNoLogin']
-        return (store.state.users.currentUser? redirectRootLogin: redirectRootNoLogin) || '/main/all'
+      { name: 'root',
+        path: '/',
+        redirect: to => {
+          var redirectRootLogin = data['redirectRootLogin']
+          var redirectRootNoLogin = data['redirectRootNoLogin']
+          return (store.state.users.currentUser ? redirectRootLogin : redirectRootNoLogin) || '/main/all'
       }},
       { path: '/main/all', component: PublicAndExternalTimeline },
       { path: '/main/public', component: PublicTimeline },

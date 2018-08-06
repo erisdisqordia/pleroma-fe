d5efa10946239dc1c9e0aa646dc1238a2737c456
Henry Jameson <me@hjkos.com>
2018-08-06 09:52:07 +0300

    hopefully, fix linter

--- src/components/post_status_form/post_status_form.js
+++ src/components/post_status_form/post_status_form.js
@@ -87,10 +87,10 @@ const PostStatusForm = {
           return false
         }
         return map(take(matchedEmoji, 5), ({shortcode, image_url, utf}, index) => ({
-          // eslint-disable-next-line camelcase
           screen_name: `:${shortcode}:`,
           name: '',
           utf: utf || '',
+          // eslint-disable-next-line camelcase
           img: utf ? '' : this.$store.state.config.server + image_url,
           highlighted: index === this.highlighted
         }))

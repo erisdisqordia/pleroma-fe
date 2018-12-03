6b3489a4d30b6530091919b4c44617a3294ffdd2
Henry Jameson <me@hjkos.com>
2018-12-03 08:03:11 +0300

    search button for user finder

--- src/components/user_finder/user_finder.vue
+++ src/components/user_finder/user_finder.vue
@@ -2,10 +2,13 @@
   <span class="user-finder-container">
     <i class="icon-spin4 user-finder-icon animate-spin-slow" v-if="loading" />
     <a href="#" v-if="hidden"><i class="icon-user-plus user-finder-icon" @click.prevent.stop="toggleHidden"/></a>
-    <span v-else>
+    <template v-else>
       <input class="user-finder-input" @keyup.enter="findUser(username)" v-model="username" :placeholder="$t('finder.find_user')" id="user-finder-input" type="text"/>
+      <button class="btn search-button" @click="findUser(username)">
+        <i class="icon-search"/>
+      </button>
       <i class="icon-cancel user-finder-icon" @click.prevent.stop="toggleHidden"/>
-    </span>
+    </template>
   </span>
 </template>
 
@@ -15,13 +18,24 @@
 @import '../../_variables.scss';
 
 .user-finder-container {
-  height: 29px;
   max-width: 100%;
-}
+  display: inline-flex;
+  align-items: baseline;
+  vertical-align: baseline;
 
-.user-finder-input {
-  max-width: 80%;
-  vertical-align: middle;
+
+  .user-finder-input,
+  .search-button {
+    height: 29px;
+  }
+  .user-finder-input {
+    max-width: 80%;
+  }
+
+  .search-button {
+    margin-left: .5em;
+    margin-right: .5em;
+  }
 }
 
 </style>

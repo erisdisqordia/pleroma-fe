47b0b385f41d925d38642329937423cf0022865d
Henry Jameson <me@hjkos.com>
2018-12-05 11:37:01 +0300

    Added user option to hide instance-specific panel, rearranged config screen to better categorize it / adjustments to language selector

--- src/components/instance_specific_panel/instance_specific_panel.js
+++ src/components/instance_specific_panel/instance_specific_panel.js
@@ -2,6 +2,9 @@ const InstanceSpecificPanel = {
   computed: {
     instanceSpecificPanelContent () {
       return this.$store.state.instance.instanceSpecificPanelContent
+    },
+    show () {
+      return !this.$store.state.config.hideISP
     }
   }
 }

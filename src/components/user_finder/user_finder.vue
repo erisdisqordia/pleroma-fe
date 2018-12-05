403c86e4d1b0067634d1f0f6bd3d6908f8b4481b
Henry Jameson <me@hjkos.com>
2018-12-05 15:01:56 +0300

    back button for mobile

--- src/components/user_finder/user_finder.vue
+++ src/components/user_finder/user_finder.vue
@@ -1,12 +1,12 @@
 <template>
-  <span class="user-finder-container">
+  <div class="user-finder-container">
     <i class="icon-spin4 user-finder-icon animate-spin-slow" v-if="loading" />
     <a href="#" v-if="hidden"  :title="$t('finder.find_user')" ><i class="icon-user-plus user-finder-icon" @click.prevent.stop="toggleHidden" /></a>
     <span v-else>
       <input class="user-finder-input" @keyup.enter="findUser(username)" v-model="username" :placeholder="$t('finder.find_user')" id="user-finder-input" type="text"/>
       <i class="icon-cancel user-finder-icon" @click.prevent.stop="toggleHidden"/>
     </span>
-  </span>
+  </div>
 </template>
 
 <script src="./user_finder.js"></script>
@@ -15,7 +15,6 @@
 @import '../../_variables.scss';
 
 .user-finder-container {
-  height: 29px;
   max-width: 100%;
 }
 

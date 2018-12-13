42bb34821908430cdfdbad827567049e3bf22b22
Henry Jameson <me@hjkos.com>
2018-12-13 15:34:51 +0300

    Fix fetching new users, add storing local users in usersObjects with their screen_name as well as id, so that they could be fetched zero-state with screen-name link.

--- src/modules/users.js
+++ src/modules/users.js
@@ -16,6 +16,9 @@ export const mergeOrAdd = (arr, obj, item) => {
     // This is a new item, prepare it
     arr.push(item)
     obj[item.id] = item
+    if (item.screen_name && !item.screen_name.includes('@')) {
+      obj[item.screen_name] = item
+    }
     return {item, new: true}
   }
 }
@@ -78,7 +81,7 @@ const users = {
   actions: {
     fetchUser (store, id) {
       store.rootState.api.backendInteractor.fetchUser({id})
-        .then((user) => store.commit('addNewUsers', user))
+        .then((user) => store.commit('addNewUsers', [user]))
     },
     addNewStatuses (store, { statuses }) {
       const users = map(statuses, 'user')

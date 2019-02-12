f2ce2e917623e131741bd0dc163734f416ff83f7
dave <starpumadev@gmail.com>
2019-02-11 22:35:24 -0500

    #344 - replace float: with flexbox, clean up

--- src/components/post_status_form/post_status_form.vue
+++ src/components/post_status_form/post_status_form.vue
@@ -118,6 +118,14 @@
   }
 }
 
+.post-status-form {
+  .visibility-tray {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+  }
+}
+
 .post-status-form, .login {
   .form-bottom {
     display: flex;

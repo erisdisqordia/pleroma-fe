8923492e6824ad43731930dfc3b57dba45cc52e9
taehoon <th.dev91@gmail.com>
2019-02-25 22:57:40 -0500

    Remove needless div wrapper

--- src/components/follow_card/follow_card.vue
+++ src/components/follow_card/follow_card.vue
@@ -1,33 +1,31 @@
 <template>
   <basic-user-card :user="user">
     <template slot="secondary-area">
-      <div class="follow-box">
-        <button
-          v-if="showFollow"
-          class="btn btn-default"
-          @click="followUser"
-          :disabled="progress"
-          :title="requestSent ? $t('user_card.follow_again') : ''"
-        >
-          <template v-if="progress">
-            {{ $t('user_card.follow_progress') }}
-          </template>
-          <template v-else-if="requestSent">
-            {{ $t('user_card.follow_sent') }}
-          </template>
-          <template v-else>
-            {{ $t('user_card.follow') }}
-          </template>
-        </button>
-        <button v-if="following" class="btn btn-default pressed" @click="unfollowUser" :disabled="progress">
-          <template v-if="progress">
-            {{ $t('user_card.follow_progress') }}
-          </template>
-          <template v-else>
-            {{ $t('user_card.follow_unfollow') }}
-          </template>
-        </button>
-      </div>
+      <button
+        v-if="showFollow"
+        class="btn btn-default"
+        @click="followUser"
+        :disabled="progress"
+        :title="requestSent ? $t('user_card.follow_again') : ''"
+      >
+        <template v-if="progress">
+          {{ $t('user_card.follow_progress') }}
+        </template>
+        <template v-else-if="requestSent">
+          {{ $t('user_card.follow_sent') }}
+        </template>
+        <template v-else>
+          {{ $t('user_card.follow') }}
+        </template>
+      </button>
+      <button v-if="following" class="btn btn-default pressed" @click="unfollowUser" :disabled="progress">
+        <template v-if="progress">
+          {{ $t('user_card.follow_progress') }}
+        </template>
+        <template v-else>
+          {{ $t('user_card.follow_unfollow') }}
+        </template>
+      </button>
     </template>
     <template slot="third-area">
       <span class="faint" v-if="!noFollowsYou && user.follows_you">

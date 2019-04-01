487a194f6d83472c57588b6c8e46b52dd4d3e2f7
jared <jaredrmain@gmail.com>
2019-04-01 13:01:28 -0400

    #470 - handle cases without subject

--- src/components/status/status.js
+++ src/components/status/status.js
@@ -253,6 +253,9 @@ const Status = {
       return this.$store.state.config.maxThumbnails
     },
     contentHtml () {
+      if (!this.status.summary_html) {
+        return this.status.statusnet_html
+      }
       return this.status.summary_html + '<br />' + this.status.statusnet_html
     }
   },

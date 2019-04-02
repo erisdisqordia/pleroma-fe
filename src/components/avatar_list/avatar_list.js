df68ce4d83a7dfef0130d10e122f3523f95c7073
Brenden Bice <brenden.next@gmail.com>
2019-04-02 12:02:01 -0400

    set maximum avatars count to 15

--- src/components/avatar_list/avatar_list.js
+++ src/components/avatar_list/avatar_list.js
@@ -4,7 +4,7 @@ const AvatarList = {
   props: ['avatars'],
   computed: {
     slicedAvatars () {
-      return this.avatars ? this.avatars.slice(0, 10) : []
+      return this.avatars ? this.avatars.slice(0, 15) : []
     }
   },
   components: {

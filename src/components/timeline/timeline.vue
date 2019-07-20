d7dc07066734fc69ef01022f92f57ededbbd0777
taehoon <th.dev91@gmail.com>
2019-07-19 22:46:11 -0400

    put two loops in one parent

--- src/components/timeline/timeline.vue
+++ src/components/timeline/timeline.vue
@@ -31,15 +31,13 @@
         <template v-for="statusId in pinnedStatusIds">
           <conversation
             v-if="timeline.statusesObject[statusId]"
-            :key="statusId"
+            :key="statusId + '-pinned'"
             class="status-fadein"
             :statusoid="timeline.statusesObject[statusId]"
             :collapsable="true"
             :show-pinned="true"
           />
         </template>
-      </div>
-      <div class="timeline">
         <conversation
           v-for="status in statuses"
           :key="status.id"

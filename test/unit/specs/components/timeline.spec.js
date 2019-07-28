18a41e785ed85531f032a0aa1b6bfdce5a892fa9
taehoon <th.dev91@gmail.com>
2019-07-28 16:55:34 -0400

    update unit test

--- test/unit/specs/components/timeline.spec.js
+++ test/unit/specs/components/timeline.spec.js
@@ -4,17 +4,15 @@ describe('Timeline', () => {
   describe('getExcludedStatusIdsByPinning', () => {
     const mockStatuses = (ids) => ids.map(id => ({ id }))
 
-    it('should not return any unpinned status ids', () => {
-      const statuses = mockStatuses([1, 2, 3, 4])
-      const pinnedStatusIds = [1, 3, 5]
-      expect(pinnedStatusIds).to.include.members(getExcludedStatusIdsByPinning(statuses, pinnedStatusIds))
-    })
-
-    it('should not return any status ids not listed in the given statuses', () => {
+    it('should return only members of both pinnedStatusIds and ids of the given statuses', () => {
       const statusIds = [1, 2, 3, 4]
       const statuses = mockStatuses(statusIds)
       const pinnedStatusIds = [1, 3, 5]
-      expect(statusIds).to.include.members(getExcludedStatusIdsByPinning(statuses, pinnedStatusIds))
+      const result = getExcludedStatusIdsByPinning(statuses, pinnedStatusIds)
+      result.forEach(item => {
+        expect(item).to.be.oneOf(statusIds)
+        expect(item).to.be.oneOf(pinnedStatusIds)
+      })
     })
 
     it('should return ids of pinned statuses not posted before any unpinned status', () => {

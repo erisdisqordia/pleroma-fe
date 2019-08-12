0bce615fe8bd0c219c66c2bc6459444df5f52ff5
taehoon <th.dev91@gmail.com>
2019-08-12 14:29:11 -0400

    hide ISP if the panel content is empty

--- src/components/instance_specific_panel/instance_specific_panel.vue
+++ src/components/instance_specific_panel/instance_specific_panel.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    v-if="show"
-    class="instance-specific-panel"
-  >
+  <div class="instance-specific-panel">
     <div class="panel panel-default">
       <div class="panel-body">
         <!-- eslint-disable vue/no-v-html -->
@@ -14,6 +11,3 @@
 </template>
 
 <script src="./instance_specific_panel.js" ></script>
-
-<style lang="scss">
-</style>

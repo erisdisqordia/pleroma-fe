0df0876cf1a8f961c8df12a7f0d11d9a61e64963
taehoon <th.dev91@gmail.com>
2019-10-18 07:07:16 -0400

    move modal css into the correct place

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -21,6 +21,20 @@ export default {
 <style lang="scss">
 
 .modal-view {
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+  animation-duration: 0.2s;
+  background-color: rgba(0, 0, 0, 0.5);
+  animation-name: modal-background-fadein;
+
   body:not(.scroll-locked) & {
     display: none;
   }

54f692622ad4fe6c427ae6d67816be93a7644ac8
taehoon <th.dev91@gmail.com>
2019-12-15 14:29:45 -0500

    wire up staff accounts with correct store data

--- src/components/staff_panel/staff_panel.js
+++ src/components/staff_panel/staff_panel.js
@@ -1,3 +1,4 @@
+import map from 'lodash/map'
 import BasicUserCard from '../basic_user_card/basic_user_card.vue'
 
 const StaffPanel = {
@@ -6,7 +7,7 @@ const StaffPanel = {
   },
   computed: {
     staffAccounts () {
-      return this.$store.state.instance.staffAccounts
+      return map(this.$store.state.instance.staffAccounts, nickname => this.$store.getters.findUser(nickname)).filter(_ => _)
     }
   }
 }

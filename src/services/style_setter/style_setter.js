e6f148b8a306bc24a2fa47da468c4e29b1fcb018
Henry Jameson <me@hjkos.com>
2020-02-11 00:56:51 +0200

    removed base16-related code

--- src/services/style_setter/style_setter.js
+++ src/services/style_setter/style_setter.js
@@ -1,57 +1,6 @@
-import { times } from 'lodash'
 import { convert } from 'chromatism'
 import { rgb2hex, hex2rgb, rgba2css, getCssColor, relativeLuminance } from '../color_convert/color_convert.js'
 import { getColors, computeDynamicColor } from '../theme_data/theme_data.service.js'
-
-// While this is not used anymore right now, I left it in if we want to do custom
-// styles that aren't just colors, so user can pick from a few different distinct
-// styles as well as set their own colors in the future.
-
-export const setStyle = (href) => {
-  /***
-      What's going on here?
-      I want to make it easy for admins to style this application. To have
-      a good set of default themes, I chose the system from base16
-      (https://chriskempson.github.io/base16/) to style all elements. They
-      all have the base00..0F classes. So the only thing an admin needs to
-      do to style Pleroma is to change these colors in that one css file.
-      Some default things (body text color, link color) need to be set dy-
-      namically, so this is done here by waiting for the stylesheet to be
-      loaded and then creating an element with the respective classes.
-
-      It is a bit weird, but should make life for admins somewhat easier.
-  ***/
-  const head = document.head
-  const body = document.body
-  body.classList.add('hidden')
-  const cssEl = document.createElement('link')
-  cssEl.setAttribute('rel', 'stylesheet')
-  cssEl.setAttribute('href', href)
-  head.appendChild(cssEl)
-
-  const setDynamic = () => {
-    const baseEl = document.createElement('div')
-    body.appendChild(baseEl)
-
-    let colors = {}
-    times(16, (n) => {
-      const name = `base0${n.toString(16).toUpperCase()}`
-      baseEl.setAttribute('class', name)
-      const color = window.getComputedStyle(baseEl).getPropertyValue('color')
-      colors[name] = color
-    })
-
-    body.removeChild(baseEl)
-
-    const styleEl = document.createElement('style')
-    head.appendChild(styleEl)
-    // const styleSheet = styleEl.sheet
-
-    body.classList.remove('hidden')
-  }
-
-  cssEl.addEventListener('load', setDynamic)
-}
 
 export const applyTheme = (input) => {
   const { rules } = generatePreset(input)

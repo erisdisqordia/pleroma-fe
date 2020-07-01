15d492ace4db5243330741a007daa9929e221f64
Shpuld Shpuldson <shp@cock.li>
2020-07-01 19:24:17 +0300

    revert accidental change in instance.js

--- src/modules/instance.js
+++ src/modules/instance.js
@@ -8,7 +8,7 @@ const defaultState = {
   // Stuff from apiConfig
   name: 'Pleroma FE',
   registrationOpen: true,
-  server: 'http://lain.com:4040',
+  server: 'http://localhost:4040/',
   textlimit: 5000,
   themeData: undefined,
   vapidPublicKey: undefined,

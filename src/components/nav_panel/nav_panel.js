77bb31b474f16aa2bbda66e2aa73cf0d20eb934e
Shpuld Shpuldson <shp@cock.li>
2020-07-03 13:26:11 +0300

    remove extra computed from nav panel

--- src/components/nav_panel/nav_panel.js
+++ src/components/nav_panel/nav_panel.js
@@ -19,7 +19,6 @@ const NavPanel = {
     },
     ...mapState({
       currentUser: state => state.users.currentUser,
-      chat: state => state.chat.channel,
       followRequestCount: state => state.api.followRequests.length,
       privateMode: state => state.instance.private,
       federating: state => state.instance.federating

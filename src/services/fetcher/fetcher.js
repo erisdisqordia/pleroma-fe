d939f2ffbcb632361a0362f0f2049f99160dee64
Shpuld Shpuldson <shp@cock.li>
2020-09-02 21:08:06 +0300

    document makeFetcher a bit

--- src/services/fetcher/fetcher.js
+++ src/services/fetcher/fetcher.js
@@ -1,11 +1,17 @@
 
-export const makeFetcher = (call, interval) => {
+// makeFetcher - replacement for setInterval for fetching, starts counting
+// the interval only after a request is done instead of immediately.
+// promiseCall is a function that returns a promise, it's called when created
+// and after every interval.
+// interval is the interval delay in ms.
+
+export const makeFetcher = (promiseCall, interval) => {
   let stopped = false
   let timeout = null
   let func = () => {}
 
   func = () => {
-    call().finally(() => {
+    promiseCall().finally(() => {
       if (stopped) return
       timeout = window.setTimeout(func, interval)
     })

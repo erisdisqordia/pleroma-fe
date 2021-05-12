dfb9bf63dedfc770db400c40b51882182a913db9
Eris <femmediscord@gmail.com>
2021-05-12 06:31:43 +0000

    Match timeline menu order with stable

--- src/components/timeline_menu/timeline_menu_content.vue
+++ src/components/timeline_menu/timeline_menu_content.vue
@@ -12,30 +12,6 @@
         />{{ $t("nav.home_timeline") }}
       </router-link>
     </li>
-    <li v-if="currentUser || !privateMode">
-      <router-link
-        class="menu-item"
-        :to="{ name: 'public-timeline' }"
-      >
-        <FAIcon
-          fixed-width
-          class="fa-scale-110 fa-old-padding "
-          icon="users"
-        />{{ $t("nav.public_tl") }}
-      </router-link>
-    </li>
-    <li v-if="federating && (currentUser || !privateMode)">
-      <router-link
-        class="menu-item"
-        :to="{ name: 'public-external-timeline' }"
-      >
-        <FAIcon
-          fixed-width
-          class="fa-scale-110 fa-old-padding "
-          icon="globe"
-        />{{ $t("nav.twkn") }}
-      </router-link>
-    </li>
     <li v-if="currentUser">
       <router-link
         class="menu-item"
@@ -58,6 +34,30 @@
           class="fa-scale-110 fa-old-padding "
           icon="envelope"
         />{{ $t("nav.dms") }}
+      </router-link>
+    </li>
+    <li v-if="currentUser || !privateMode">
+      <router-link
+        class="menu-item"
+        :to="{ name: 'public-timeline' }"
+      >
+        <FAIcon
+          fixed-width
+          class="fa-scale-110 fa-old-padding "
+          icon="users"
+        />{{ $t("nav.public_tl") }}
+      </router-link>
+    </li>
+    <li v-if="federating && (currentUser || !privateMode)">
+      <router-link
+        class="menu-item"
+        :to="{ name: 'public-external-timeline' }"
+      >
+        <FAIcon
+          fixed-width
+          class="fa-scale-110 fa-old-padding "
+          icon="globe"
+        />{{ $t("nav.twkn") }}
       </router-link>
     </li>
   </ul>
